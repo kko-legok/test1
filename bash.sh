77f79a4d8d3be193e09d1bd52a7b2b916f704724
KKO-LEGOK <alancuapgo@gmail.com>
2025-10-01 20:24:43 +0700

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: Kuis Online Firestore"
+git branch -M main
+git remote add origin https://github.com/username/kuis-online-firestore.git
+git push -u origin main
